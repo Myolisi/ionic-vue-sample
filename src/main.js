068a9531c84c2220f8fca148c447c673e33a8505
myolisi <myolisi@ngubentombi.co.za>
2020-02-09 11:30:35 +0200

    Add capacitor

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Ionic from "@ionic/vue";
+import "@ionic/core/css/core.css";
+import "@ionic/core/css/ionic.bundle.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Ionic);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Gigle-dee</ion-title>
+        <ion-icon slot="end" name="hammer"></ion-icon>
+      </ion-toolbar>
+    </ion-header>
+    <ion-grid fixed>
+      <ion-row></ion-row>
+      <ion-row class="ion-padding-bottom">
+        <ion-col size="12">Just hit the button and have fun</ion-col>
+      </ion-row>
+      <ion-row>
+        <ion-col size="12">
+          <ion-button expand="block" v-on:click="generateJoke()" fill="solid">Get Joke</ion-button>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
+    <!-- Card results -->
+    <ion-progress-bar v-if="doSearch" type="indeterminate"></ion-progress-bar>
+    <ion-card v-if="jokes && !doSearch">
+      <ion-card-header>
+        <ion-card-title>{{jokes['setup']}}</ion-card-title>
+      </ion-card-header>
+      <ion-card-content>{{jokes['punchline']}}</ion-card-content>
+    </ion-card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  data() {
+    return {
+      doSearch: false,
+      jokes: ""
+    };
+  },
+  methods: {
+    generateJoke() {
+      this.doSearch = true;
+      axios
+        .get("https://official-joke-api.appspot.com/random_joke")
+        .then(res => {
+          this.doSearch = false;
+          this.jokes = res.data;
+          console.log(this.jokes);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  },
+  mounted() {}
+};
 </script>
